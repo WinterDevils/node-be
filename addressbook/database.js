cad47cac095e39d3c2750abb25db2944e3e580fe
HeHui <limhehui@maxis.com.my>
2022-01-22 15:21:15 +0800

    Adding database migration

--- addressbook/database.js
+++ addressbook/database.js
@@ -1,26 +1,44 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
-                                process.env.DB_USER || 'postgres',
-                                process.env.DB_PASSWORD || '',
-                                {
-                                    host: process.env.DB_HOST || 'localhost',
-                                    port: process.env.DB_PORT || 5432,
-                                    dialect: 'postgres',
-                                    dialectOptions: {
-                                        ssl: process.env.DB_SSL == "true"
-                                    }
-                                });
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres', process.env.DB_USER || 'postgres', process.env.DB_PASSWORD || '', {
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: process.env.DB_SSL == 'true',
+  },
+})
 const Person = sequelize.define('Person', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+})
+const Profile = sequelize.define(
+  'Profile',
+  {
     firstName: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     lastName: {
-        type: Sequelize.STRING,
-        allowNull: true
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-});
+    address: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    paranoid: true,
+  }
+)
 module.exports = {
-    sequelize: sequelize,
-    Person: Person
-};
+  sequelize: sequelize,
+  Person: Person,
+  Profile: Profile,
+}
