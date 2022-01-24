4d8977823c8143e0b8296928469498b400632455
HeHui <limhehui@maxis.com.my>
2022-01-24 22:18:55 +0800

    Making API with address work

--- addressbook/routes/profile.js
+++ addressbook/routes/profile.js
@@ -1,11 +1,10 @@
-// tutorial
-
 var express = require('express')
 var router = express.Router()
+var moment = require('moment')
+const uuid1 = require('uuid/v1')
+const { Address } = require('../database')
 
-// var bodyParser = require('body-parser');
 var db = require('../database')
-// var app = express();
 
 router.get('/', function (req, res) {
   db.Profile.findAll()
@@ -14,13 +13,19 @@ router.get('/', function (req, res) {
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || 'Some error occurred while retrieving tutorials.',
+        message: err.message || 'Some error occurred while retrieving profiles.',
       })
     })
 })
 
 router.get('/:id', function (req, res) {
-  db.Profile.findByPk(req.params.id)
+  const id = req.params.id
+  db.Profile.findAll({
+    where: { uuid: id },
+    include: {
+      model: db.Address,
+    },
+  })
     .then((profile) => {
       res.status(200).send(JSON.stringify(profile))
     })
@@ -29,63 +34,90 @@ router.get('/:id', function (req, res) {
     })
 })
 
-router.post('/', function (req, res) {
-  db.Profile.create(
-    {
-      mrn: req.body.mrn,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      // dob: req.body.dob,
-      gender: req.body.gender,
-      contactNo: req.body.contactNo,
-      email: req.body.email,
-      isActive: req.body.isActive,
-      addresses: {
+router.post('/', async function (req, res) {
+  const uniqueId = uuid1()
+  const t = await db.sequelize.transaction()
+
+  try {
+    await db.Profile.create(
+      {
+        uuid: uniqueId,
+        mrn: req.body.mrn,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        dob: moment(req.body.dob, 'DD-MM-YYYY'),
+        gender: req.body.gender,
+        contactNo: req.body.contactNo,
+        email: req.body.email,
+        isActive: req.body.isActive,
+      },
+      { transaction: t }
+    )
+    await db.Address.create(
+      {
         one: req.body.address.one,
         two: req.body.address.two,
-        state: req.body.address.state,
         postcode: req.body.address.postcode,
+        state: req.body.address.state,
+        lat: req.body.address.lat,
+        long: req.body.address.long,
+        ProfileUuid: uniqueId,
       },
-    },
-    {
-      include: [{ association: db.Profile.Addresses, include: [db.Profile.Addresses] }],
-    }
-  )
-    .then((profile) => {
-      res.status(200).send(JSON.stringify(profile))
-    })
-    .catch((err) => {
-      res.status(500).send(JSON.stringify(err))
-    })
+      { transaction: t }
+    )
+
+    await t.commit()
+    res.status(200).send(JSON.stringify(`Profile with ID:${uniqudId} has been created`))
+  } catch (err) {
+    await t.rollback()
+    res.status(504).send(JSON.stringify(err))
+  }
 })
 
-router.put('/:id', function (req, res) {
+router.put('/:id', async function (req, res) {
   const id = req.params.id
-  db.Profile.update(req.body, {
-    where: { id: id },
-  })
-    .then((num) => {
-      if (num == 1) {
-        res.send({
-          message: `Profile ${id} was updated successfully.`,
-        })
-      } else {
-        res.send({
-          message: `Cannot update profile with id=${id}. Maybe profile was not found or req.body is empty!`,
-        })
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: 'Error updating profile with id=' + id,
-      })
-    })
+  const t = await db.sequelize.transaction()
+
+  try {
+    await db.Profile.update(
+      {
+        mrn: req.body.mrn,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        dob: moment(req.body.dob, 'DD-MM-YYYY'),
+        gender: req.body.gender,
+        contactNo: req.body.contactNo,
+        email: req.body.email,
+        isActive: req.body.isActive,
+      },
+      { where: { uuid: id }, transaction: t }
+    )
+
+    await db.Address.update(
+      {
+        one: req.body.address.one,
+        two: req.body.address.two,
+        postcode: req.body.address.postcode,
+        state: req.body.address.state,
+        lat: req.body.address.lat,
+        long: req.body.address.long,
+        ProfileUuid: id,
+      },
+      { where: { ProfileUuid: id }, transaction: t }
+    )
+
+    await t.commit()
+    res.status(200).send(JSON.stringify(`Profile with ID:${id} has been updated successfully.`))
+  } catch (err) {
+    await t.rollback()
+    res.status(504).send(JSON.stringify(err))
+  }
 })
 
 router.delete('/:id', function (req, res) {
   const id = req.params.id
   db.Profile.destroy({
-    where: { id: id },
+    where: { uuid: id },
   })
     .then((num) => {
       if (num == 1) {
